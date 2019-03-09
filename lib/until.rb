2ffdeadec5c2b40402ffa5ec5b26fc10a4661ff8
Ay Has <ahashim983@yahoo.com>
2019-03-09 12:02:45 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,11 @@
-levitation_force = 6
-def using_until(levitation_force)
+
+def using_until
+  levitation_force = 6
   until levitation_force == 10
     puts "Wingardium Leviosa"
     levitation_force += 1
   end
 end
 
-using_until(levitation_force)
+using_until
   #your code here
