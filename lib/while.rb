d92f23c55f998eb0521f53a76fa284cb5a8e368e
Ay Has <ahashim983@yahoo.com>
2019-03-09 11:57:09 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,10 @@
 
 levitation_force = 6
 
-  while levitation_force < 10
-    puts "Wingardium Leviosa"
-    levitation_force = levitation_force + 1
-  end
+while levitation_force < 10
+  puts "Wingardium Leviosa"
+  levitation_force = levitation_force + 1
+end
 
 
   #your code here
